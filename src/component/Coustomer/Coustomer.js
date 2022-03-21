f8f108f0ee870b9066587b5c90ba2ba888c3b3a6
pallobbiswas <pallobbiswas326@gmail.com>
2022-03-21 10:57:52 +0600

    added done

--- src/component/Coustomer/Coustomer.js
+++ src/component/Coustomer/Coustomer.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { addToCart } from "../../utilities/fakeDb";
 import "./Coustomer.css";
 
 const Coustomer = (props) => {
   const { name, gender, id } = props.coustomer;
+
+  //add to cart
+  const orderBtn = (id) => {
+      addToCart(id)
+  }
+
+
   return (
     <div className="coustomer">
       <h2>Name: {name}</h2>
       <h4>Gender: {gender}</h4>
       <h4>Id: {id}</h4>
-      <button>place to order</button>
+      <button onClick={() => orderBtn(id)}>place to order</button>
       <button>Delete from cart</button>
     </div>
   );
--- src/utilities/fakeDb.js
+++ src/utilities/fakeDb.js
@@ -1,10 +1,24 @@
 // add product to card
+const addToCart = (id) => {
+  let shoppingCart = {};
 
+  //get cart
 
+  const getCart = localStorage.getItem("cart-order");
+  if (getCart) {
+    shoppingCart = JSON.parse(getCart);
+  }
+  //add count
+  const count = shoppingCart[id];
+  if (count) {
+    const newCount = +count + 1;
+    shoppingCart[id] = newCount;
+  } else {
+    shoppingCart[id] = 1;
+  }
 
-
-
-
+  localStorage.setItem("cart-order", JSON.stringify(shoppingCart));
+};
 
 export { addToCart };
 
